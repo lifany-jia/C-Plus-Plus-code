99a9e7a8cbe47e90449984c2a3916caba5531f99
lifany <lifany2008@outlook.com>
2026-02-08 21:56:22 +0800

    完成information.h和information.cpp部分

---
+++ Student Management System/Student Management System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Student Management System
+//
+//  Created by lifany on 2026/1/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
